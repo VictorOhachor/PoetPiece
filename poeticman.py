3cfaf26077ea1f04d6466393091cd9ac0622f999
VictorOhachor <victor.ohachor@kibo.school>
2023-02-19 18:22:54 +0100

    Set render_as_batch to false in flask migrate

--- poeticman.py
+++ poeticman.py
@@ -4,7 +4,7 @@ from app.models import User, Admin, Poem, Category, Stanza, Comment
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db, render_as_batch=True)
+migrate = Migrate(app, db)
 
 @app.context_processor
 
