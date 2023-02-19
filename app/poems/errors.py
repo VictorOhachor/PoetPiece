f068ec625ca295d24fb8fa5c0d402b30797192ba
VictorOhachor <victor.ohachor@kibo.school>
2023-02-19 17:09:30 +0100

    Updated:
    - Project structure to use Flask Blueprints
    
    - Made the toast message much more smart to figure out when a message is a success message or caused by an error

--- app/poems/errors.py
+++ app/poems/errors.py
@@ -3,9 +3,9 @@ from . import poems
 
 @poems.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @poems.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500,html'), 500
+    return render_template('errors/500,html'), 500
