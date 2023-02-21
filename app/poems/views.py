d6a1a91140773f1c32435c200d65f8e9e9b471b1
VictorOhachor <victor.ohachor@kibo.school>
2023-02-21 11:17:33 +0100

    Fixed bug in /poem/<poem_id/[edit|delete] routes

--- app/poems/views.py
+++ app/poems/views.py
@@ -105,7 +105,8 @@ def poem(poem_id):
     }
 
     # Get poem stanzas order by stanza index
-    context['stanzas'] = context['poem'].stanzas.order_by(Stanza.index).all()
+    context['stanzas'] = (Stanza.query.filter_by(poem_id=poem_id)
+                          .order_by(Stanza.index).all())
 
     if not context['poem'].published:
         if current_user.is_anonymous or not current_user.is_admin:
