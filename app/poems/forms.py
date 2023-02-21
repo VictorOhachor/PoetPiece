dea9572fc1395f9219e70d58bf0a82069c1d39e2
VictorOhachor <victor.ohachor@kibo.school>
2023-02-21 12:38:42 +0100

    Removed:
    - DataRequired validation from poem form description

--- app/poems/forms.py
+++ app/poems/forms.py
@@ -11,8 +11,8 @@ class CreatePoemForm(FlaskForm):
 
     title = StringField('New Poem Title',
                         validators=[DataRequired(), Length(1, 255)])
-    description = TextAreaField('Describe the New Poem', validators=[
-        DataRequired(), Length(1, 600), ])
+    description = TextAreaField('Describe the New Poem',
+                                validators=[Length(1, 600), ])
     category = SelectField('Select Poem Category',
                            coerce=str, validators=[DataRequired()])
     is_premium = BooleanField('Is this a premium poem?')
