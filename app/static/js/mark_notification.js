e943866880fffa1a1c46018ab87931edd38a4b50
VictorOhachor <victor.ohachor@kibo.school>
2023-04-16 02:56:00 +0100

    Modified:
    - The UI of the notifications menu page to look more appealing and user-friendly
    
    Added:
    
    - The ability for users to mark a notification as read or unread

--- app/static/js/mark_notification.js
+++ app/static/js/mark_notification.js
@@ -1,13 +1,15 @@
 const notificationMarkers = document.querySelectorAll('.unread-checkbox')
 
-for (const marker of notificationMarkers) {
-  marker.addEventListener('click', (e) => {
-    e.preventDefault()
+const mark_notification = (e) => {
+  e.preventDefault()
 
-    // retrieve data attribute that contains the endpoint to visit in order
-    // to mark a notification as read or unread
-    const { markEndpoint } = e.currentTarget.dataset
-    // visit endpoint
-    window.location.pathname = markEndpoint
-  })
+  // retrieve data attribute that contains the endpoint to visit in order
+  // to mark a notification as read or unread
+  const { markEndpoint } = e.currentTarget.dataset
+  // visit endpoint
+  window.location.pathname = markEndpoint
+}
+
+for (const marker of notificationMarkers) {
+  marker.addEventListener('click', mark_notification)
 }
