8bdf980cbb6f2f14aa6f9977fa764c0f58f9c331
VictorOhachor <victor.ohachor@kibo.school>
2023-05-12 06:33:41 +0100

    Fixed the delete_img bug that prevents it from deleting an image

--- app/resources/manager.py
+++ app/resources/manager.py
@@ -56,7 +56,7 @@ class ResourceManager:
         # Extract the filename from the file path
         filename = filepath.rpartition('/')[-1]
         # Generate an absolute path to the image
-        img_path = os.path.join(upload_folder, f'resources/{filename}')
+        img_path = os.path.join(upload_folder, f'{dirname}/{filename}')
 
         if os.path.exists(img_path):
             os.remove(img_path)
